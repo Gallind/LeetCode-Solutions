bf3827eefc6e901678fe1e0cb250920a8403e4e0
Gal Lind <43096487+Gallind@users.noreply.github.com>
2025-08-06 15:55:19 +0300

    Time: 2 ms (96.97%), Space: 43.5 MB (35.52%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,22 +1,39 @@
 import java.util.ArrayList;
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> inter = new ArrayList<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int rep = -1;
+        Set<Integer> nums = new HashSet<>();
+        Set<Integer> inter = new HashSet<>();
         for (int num : nums1){
-            if (num == rep) continue;
-            rep = num;
-            int num12 = Arrays.binarySearch(nums2, num);
-            if (num12 >= 0){
-                inter.add(nums2[num12]);
+            nums.add(num);
+        }
+        for (int num : nums2){
+            if (nums.contains(num)){
+                inter.add(num);
             }
         }
         int[] output = new int[inter.size()];
-        for (int i = 0; i < inter.size(); i++){
-            output[i] = inter.get(i);
+        int i = 0;
+        for (int num : inter){
+            output[i++] = num;
         }
         return output;
     }
 }
+    //     ArrayList<Integer> inter = new ArrayList<>();
+    //     Arrays.sort(nums1);
+    //     Arrays.sort(nums2);
+    //     int rep = -1;
+    //     for (int num : nums1){
+    //         if (num == rep) continue;
+    //         rep = num;
+    //         int num12 = Arrays.binarySearch(nums2, num);
+    //         if (num12 >= 0){
+    //             inter.add(nums2[num12]);
+    //         }
+    //     }
+    //     int[] output = new int[inter.size()];
+    //     for (int i = 0; i < inter.size(); i++){
+    //         output[i] = inter.get(i);
+    //     }
+    //     return output;
+    // }
