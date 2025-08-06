55c007976ab4bfe1c082a9a153a4a023ae266c03
Gal Lind <43096487+Gallind@users.noreply.github.com>
2025-08-06 09:36:16 +0300

    Time: 21 ms (19.8%), Space: 58.4 MB (45.73%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        if (n <= 1) return false;
+        for (int i = 0; i < n - 1; i++){
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+}
