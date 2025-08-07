c1ed7f913a9f9f5f8f493d1ef0fd76131d38dd11
Gal Lind <43096487+Gallind@users.noreply.github.com>
2025-08-07 15:22:21 +0300

    Time: 0 ms (100%), Space: 42.4 MB (9.57%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        int carry = 1;
+        for (int i = len - 1; i >= 0; i--){
+            if (carry == 0) break;
+            digits[i] += carry;
+            if (digits[i] >= 10){
+                digits[i] = 0;
+            }
+            else carry = 0;
+        }
+        if (digits[0] == 0){
+            int[] sol = new int[len + 1];
+            sol[0] = 1;
+            for (int i = 1; i <= len; i++){
+                sol[i] = 0;
+            }
+            return sol;
+        }
+        return digits;
+    }
+}
