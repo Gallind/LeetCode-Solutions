48593195903015a5c86ff6a3e3b8949a953601e9
Gal Lind <43096487+Gallind@users.noreply.github.com>
2025-08-07 14:05:54 +0300

    Time: 0 ms (100%), Space: 41.7 MB (34.42%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,34 +1,40 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle == "") return 0;
-        if (haystack == "") return -1;
-        if (haystack.equals("mississippi") && needle.equals("sippia") || haystack.equals("bbaa") && needle.equals("aab")) return -1;
-        int firstIn = -1;
-        int currN = 0;
-        int lastN = needle.length() - 1;
-        boolean isNeedle = false;
+        int j = needle.length();
         int n = haystack.length();
-        if (n < needle.length()) return -1;
-        for (int i = 0; i < n; i++){
-            if (currN > lastN) return firstIn;
-            char ch = haystack.charAt(i);
-            if (!isNeedle && ch == needle.charAt(currN)){
-                isNeedle = true;
-                currN = 1;
-                firstIn = i;
-            }
-            else if (isNeedle){
-                if (ch != needle.charAt(currN)){
-                    isNeedle = false;
-                    currN = 0;
-                    i = firstIn;
-                }
-                else{
-                    currN++;
-                }
-            }
+        for (int i = 0; j <= n; i++, j++){
+            if (haystack.substring(i,j).equals(needle)) return i;
         }
-        if (!isNeedle || currN < lastN) return -1;
-        return firstIn;
+        return -1;
+        // if (needle == "") return 0;
+        // if (haystack == "") return -1;
+        // if (haystack.equals("mississippi") && needle.equals("sippia") || haystack.equals("bbaa") && needle.equals("aab")) return -1;
+        // int firstIn = -1;
+        // int currN = 0;
+        // int lastN = needle.length() - 1;
+        // boolean isNeedle = false;
+        // int n = haystack.length();
+        // if (n < needle.length()) return -1;
+        // for (int i = 0; i < n; i++){
+        //     if (currN > lastN) return firstIn;
+        //     char ch = haystack.charAt(i);
+        //     if (!isNeedle && ch == needle.charAt(currN)){
+        //         isNeedle = true;
+        //         currN = 1;
+        //         firstIn = i;
+        //     }
+        //     else if (isNeedle){
+        //         if (ch != needle.charAt(currN)){
+        //             isNeedle = false;
+        //             currN = 0;
+        //             i = firstIn;
+        //         }
+        //         else{
+        //             currN++;
+        //         }
+        //     }
+        // }
+        // if (!isNeedle || currN < lastN) return -1;
+        // return firstIn;
     }
 }
