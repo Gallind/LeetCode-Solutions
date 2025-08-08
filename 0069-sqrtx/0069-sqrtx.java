1b6147c9213563ab50ff20cce369a91f5fa8989b
Gal Lind <43096487+Gallind@users.noreply.github.com>
2025-08-08 20:15:07 +0300

    Time: 1 ms (84.36%), Space: 40.9 MB (69.24%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,10 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x < 1) return 0;
-        long i = 0;
-        while (true){
-            if (i * i <= x && (i + 1) * (i + 1) > x) return (int) i;
-            i++;
+        if (x == 0 || x == 1) return x;
+        return sqrt(x, 1, x);
+        
+    }
+    private int sqrt(int x, int l, int r){
+        long mid = (long) l + (r - l) / 2;
+        long pow = mid * mid;
+        if (l >= r){
+            if ((mid * mid) <= x) return (int) mid;
+            return (int) (mid - 1);
         }
+        if (pow == x) return (int) mid;
+        if (pow > x) return sqrt(x, l, (int)(mid - 1));
+        return sqrt(x, (int)(mid + 1), r);
     }
 }
