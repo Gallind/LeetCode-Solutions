62966d1ca458f6a4c97764ca69e0974844c8312a
Gal Lind <43096487+Gallind@users.noreply.github.com>
2025-08-16 17:59:53 +0300

    Time: 1 ms (98.59%), Space: 42.5 MB (16.27%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
@@ -1,9 +1,9 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long maxT = 0, maxE = 0, diff = 0;
+        long maxT = 0, maxE = 0, sub = 0;
         for (int num : nums){
-            maxT = Math.max(maxT, num * diff);
-            diff = Math.max(diff, maxE - num);
+            maxT = Math.max(maxT, num * sub);
+            sub = Math.max(sub, maxE - num);
             maxE = Math.max(maxE, num);
         }
         return maxT;
