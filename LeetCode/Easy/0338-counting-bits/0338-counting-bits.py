41305169abb4f684a0e0cce2a4d250214eede403
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-07 21:14:06 +0200

    Time: 7 ms (50.4%), Space: 20.3 MB (5.55%) - LeetHub

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.py
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.py
@@ -2,7 +2,6 @@ import math
 class Solution:
     def countBits(self, n: int) -> List[int]:
         if n == 0: return [0]
-        
         sol = [-1] * (n + 1)
         sol[0] = 0
         sol[1] = 1
