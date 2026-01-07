07676e84d53238eac36d74cd7bea70591e04d815
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-07 20:27:02 +0200

    Time: 0 ms (100%), Space: 19.3 MB (5.83%) - LeetHub

--- LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,15 +1,22 @@
+import math
 class Solution:
-    def isDiv(self, s: str, sub: str):
-        if not s: return True
-        if s.find(sub) != 0: return False
-        return self.isDiv(s[len(sub):], sub)
+    # def isDiv(self, s: str, sub: str):
+    #     if not s: return True
+    #     if s.find(sub) != 0: return False
+    #     return self.isDiv(s[len(sub):], sub)
         
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        max_div = ""
-        prefix = ""
-        for ch in str1:
-            prefix += ch
-            if self.isDiv(str1, prefix) and self.isDiv(str2, prefix):
-                max_div = prefix
-        return max_div
+        s1 = str1 + str2
+        s2 = str2 + str1
+        if s1 != s2:
+            return ""
+        gcd = math.gcd(len(str1), len(str2))
+        return str1[:gcd]
+        # max_div = ""
+        # prefix = ""
+        # for ch in str1:
+        #     prefix += ch
+        #     if self.isDiv(str1, prefix) and self.isDiv(str2, prefix):
+        #         max_div = prefix
+        # return max_div
         
