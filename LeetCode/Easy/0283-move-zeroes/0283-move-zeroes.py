34428c6dde3048bf4de3352f6ede3792a797b8aa
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-07 22:13:12 +0200

    Time: 3 ms (80.46%), Space: 20.6 MB (6.66%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
@@ -1,6 +1,11 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        # start = 0
+        # # end = len(nums) - 1
+        # end = 0
+
         l = 0
+        # i = 0
         for i, n in enumerate(nums):
             if n != 0:
                 nums[l] = n
@@ -9,4 +14,19 @@ class Solution:
                 l += 1
         for i in range(l + 1, len(nums)):
             nums[i] = 0
+
+        # while start <= end:
+        #     while end < len(nums) and nums[end] != 0:
+        #         end += 1
+        #     if nums[start] == 0:
+        #         nums[start], nums[end] = nums[end], nums[start]
+        #     start += 1
+
+        # while start < end:
+        #     if nums[start] == 0:
+        #         nums[start], nums[end] = nums[start + 1], nums[start]
+        #         end -= 1
+        #     else:
+        #         start += 1
+        
         
