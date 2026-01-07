51224a824dd8e53ddb45c7ea009a1fda752da45b
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-07 19:58:42 +0200

    Time: 50 ms (5.62%), Space: 19.2 MB (5.55%) - LeetHub

--- LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,14 +3,21 @@ class Solution:
         l1 = len(word1)
         l2 = len(word2)
         
-        sol = ""
-        if l1 <= l2:
-            for c1, c2 in zip(word1, word2[:l1]):
-                sol += c1 + c2
-            sol += word2[l1:]
-        else:
-            for c1, c2 in zip(word1[:l2], word2):
-                sol += c1 + c2
-            sol += word1[l2:]
-        return sol
+        sol2 = []
+        for c1, c2 in zip(word1, word2):
+            sol2.append(c1 + c2)
+        sol2.append(word1[l2:])
+        sol2.append(word2[l1:])
+        return "".join(sol2)
+
+        # sol = ""
+        # if l1 <= l2:
+        #     for c1, c2 in zip(word1, word2[:l1]):
+        #         sol += c1 + c2
+        #     sol += word2[l1:]
+        # else:
+        #     for c1, c2 in zip(word1[:l2], word2):
+        #         sol += c1 + c2
+        #     sol += word1[l2:]
+        # return sol
         
