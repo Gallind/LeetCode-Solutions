82a5d7cc301aa3dd3108fa0fb8a8e29676faec9a
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-08 13:32:28 +0200

    Time: 28 ms (31.96%), Space: 20.1 MB (8.27%) - LeetHub

---
+++ LeetCode/Medium/2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -0,0 +1,51 @@
+class SmallestInfiniteSet:
+    SMALL = 1
+    # ADDED_BACK = set()
+    # MIN = 1
+    def infGen(self):
+        x = 1
+        # x = self.MIN
+        while True:
+            while x not in self.ADDED_BACK and x < self.SMALL:
+                x += 1
+            yield x
+            x += 1
+    def __init__(self):
+        self.ADDED_BACK = set()
+        self.inf_set = self.infGen()
+        
+
+    def popSmallest(self) -> int:
+        if self.ADDED_BACK:
+            minimum = min(self.ADDED_BACK)
+            self.ADDED_BACK.discard(minimum)
+            return minimum
+        else:
+            self.SMALL += 1
+            return self.SMALL - 1
+        # if self.MIN == self.SMALL:
+        #     if self.ADDED_BACK:
+        #         self.MIN = min(self.ADDED_BACK)
+        #     else:
+        #         self.MIN += 1
+        # self.ADDED_BACK.discard(self.MIN)
+        # if self.ADDED_BACK:
+        #     self.MIN = min(min(self.ADDED_BACK), self.SMALL + 1)
+        # else:
+        #     self.MIN = self.SMALL + 1
+        #     self.SMALL += 1
+        
+        # return self.MIN - 1
+
+
+    def addBack(self, num: int) -> None:
+        # self.MIN = min(self.MIN, num)
+        if num < self.SMALL:
+            self.ADDED_BACK.add(num)
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
