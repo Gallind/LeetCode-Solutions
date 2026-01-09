40af1f53b82bc3e18a1c955ee7c433528922bd3c
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-09 17:32:23 +0200

    Time: 30 ms (15.62%), Space: 19.5 MB (5.91%) - LeetHub

---
+++ LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        self.count = 0
+        self.table = [-1] * n
+        def isSafe(col, row) -> bool:
+            for c in range(col):
+                if self.table[c] == row: return False
+                if abs(self.table[c] - row) == abs(col - c): return False
+            return True
+
+
+        def solve(col):
+            if col == n: 
+                self.count += 1
+                return
+            for row in range(n):
+                if isSafe(col, row):
+                    self.table[col] = row
+                    solve(col + 1)
+
+        solve(0)
+        return self.count
