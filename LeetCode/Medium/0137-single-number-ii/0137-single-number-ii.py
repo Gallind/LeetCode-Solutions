aaa05c111e8b2f76afe725c68c38cf5344039ff4
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-09 12:59:30 +0200

    Time: 35 ms (18.12%), Space: 20.7 MB (7.45%) - LeetHub

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.py
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.py
@@ -7,8 +7,10 @@ class Solution:
             for num in nums:
                 if num < 0:
                     num = num & ((1 << 32) - 1)
-                if (num >> i) & 1:
-                    bit_sum += 1
+                bit_sum += (num >> i) & 1
+                # if (num >> i) & 1:
+                #     bit_sum += 1
+            # res |= (bit_sum % 3) << i
             if bit_sum % 3:
                 res |= 1 << i
         if res >= 1 << 31:
