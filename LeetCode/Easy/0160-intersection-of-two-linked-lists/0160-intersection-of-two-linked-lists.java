9660819e7f2a236dcdfda9896cdc8aedeb7176a2
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-09 21:30:46 +0200

    Time: 2 ms (44.18%), Space: 53.2 MB (5.22%) - LeetHub

--- LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -14,28 +14,39 @@ public class Solution {
         int lenA = 0, lenB = 0;
         ListNode a = headA;
         ListNode b = headB;
-        while (a != null){
-            lenA++;
-            a = a.next;
+
+        if (a == null || b == null) return null;
+
+        while (a != b){
+            if (a == null) a = headB;
+            else a = a.next;
+
+            if (b == null) b = headA;
+            else b = b.next;
         }
-        while (b != null){
-            lenB++;
-            b = b.next;
-        }
-        int diff = Math.abs(lenA - lenB);
-        a = headA; 
-        b = headB;
-        if (lenA > lenB){
-            for (int i = 0; i < diff; i++) a = a.next;
-        }
-        else{
-            for (int i = 0; i < diff; i++) b = b.next;
-        }
-        while (a != null){
-            if (a == b) return a;
-            a = a.next;
-            b = b.next;
-        }
-        return null;
+        return a;
+        // while (a != null){
+        //     lenA++;
+        //     a = a.next;
+        // }
+        // while (b != null){
+        //     lenB++;
+        //     b = b.next;
+        // }
+        // int diff = Math.abs(lenA - lenB);
+        // a = headA; 
+        // b = headB;
+        // if (lenA > lenB){
+        //     for (int i = 0; i < diff; i++) a = a.next;
+        // }
+        // else{
+        //     for (int i = 0; i < diff; i++) b = b.next;
+        // }
+        // while (a != null){
+        //     if (a == b) return a;
+        //     a = a.next;
+        //     b = b.next;
+        // }
+        // return null;
     }
 }
