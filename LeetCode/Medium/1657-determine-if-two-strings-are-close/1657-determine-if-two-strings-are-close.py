88812b02c1455751d3afceac5c8925946ea91fd3
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-10 23:00:19 +0200

    Time: 175 ms (13.97%), Space: 20.7 MB (5.72%) - LeetHub

--- LeetCode/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ LeetCode/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,16 +1,13 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        freq1, freq2 = Counter(word1), Counter(word2)
-        if freq1.keys() != freq2.keys(): return False
-        return sorted(freq1.values()) == sorted(freq2.values())
-
-        # freq1 = [0] * 26
-        # freq2 = [0] * 26
-        # for c1, c2 in zip(word1, word2):
-        #     freq1[ord(c1) - ord('a')] += 1
-        #     freq2[ord(c2) - ord('a')] += 1
-        # for f1, f2 in zip(freq1, freq2):
-        #     if (f1 and not f2) or (f2 and not f1): return False
-        # freq1.sort()
-        # freq2.sort()
-        # return freq1 == freq2
+        if len(word1) != len(word2): return False
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+        for c1, c2 in zip(word1, word2):
+            freq1[ord(c1) - ord('a')] += 1
+            freq2[ord(c2) - ord('a')] += 1
+        for f1, f2 in zip(freq1, freq2):
+            if (f1 and not f2) or (f2 and not f1): return False
+        freq1.sort()
+        freq2.sort()
+        return freq1 == freq2
