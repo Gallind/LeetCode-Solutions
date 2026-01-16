e04b12cc88d732485fcc13acbbe7e9104f78e577
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-16 13:09:51 +0200

    Time: 7 ms (55.71%), Space: 19.5 MB (6.96%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -6,10 +6,12 @@ class Solution:
             ch1, ch2 = chs
             if ch1 not in dict_s:
                 dict_s[ch1] = {i,}
-            else:
-                dict_s[ch1].add(i)
+            # else:
+            #     dict_s[ch1].add(i)
             if ch2 not in dict_t:
                 dict_t[ch2] = {i,}
-            else:
-                dict_t[ch2].add(i)
-        return all(s1 == s2 for s1, s2 in zip(list(dict_s.values()), list(dict_t.values())))
+            # else:
+            #     dict_t[ch2].add(i)
+            if dict_s[ch1] != dict_t[ch2]: return False
+        return True
+        # return all(s1 == s2 for s1, s2 in zip(list(dict_s.values()), list(dict_t.values())))
