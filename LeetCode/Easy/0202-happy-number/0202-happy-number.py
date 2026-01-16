1b90432fa33c84361bed4098cd0c805520e6e701
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-16 13:27:12 +0200

    Time: 0 ms (100%), Space: 19.6 MB (10.65%) - LeetHub

---
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n == 1: return True
+        if n <= 4 or n == 9: return False
+        sum_digits = 0
+        temp = n
+        while temp > 0:
+            sum_digits += (temp % 10) ** 2
+            temp //= 10
+        print(sum_digits)
+        return self.isHappy(sum_digits)
