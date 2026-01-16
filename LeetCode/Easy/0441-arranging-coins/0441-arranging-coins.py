93d46646eb714361c6adc08174b9384d4c212988
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-16 20:59:01 +0200

    Time: 2 ms (69.27%), Space: 19.5 MB (5.8%) - LeetHub

--- LeetCode/Easy/0441-arranging-coins/0441-arranging-coins.py
+++ LeetCode/Easy/0441-arranging-coins/0441-arranging-coins.py
@@ -1,9 +1,3 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        i = 1
-        count = 0
-        while n >= i:
-            count += 1
-            n -= i
-            i += 1
-        return count
+        return int((math.sqrt(1 + n * 8) - 1) // 2)
