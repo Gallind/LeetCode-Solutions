e41db94c03a4fb1012c1a0d552ff63d6bf28f465
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-16 14:35:26 +0200

    Time: 2 ms (19.19%), Space: 19.5 MB (9.79%) - LeetHub

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,4 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        #would have solved probably using binary search but dont have the energy so...
+        return int(n:=math.sqrt(num)) == n
