049fa47db9400116c6da8552ceb8ec5648ecb401
Gal Lind <43096487+Gallind@users.noreply.github.com>
2026-01-16 13:06:15 +0200

    Time: 25 ms (5.73%), Space: 27.3 MB (6.96%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -2,16 +2,16 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         dict_s = {}
         dict_t = {}
-        for i, ch in enumerate(s):
-            if ch not in dict_s:
-                dict_s[ch] = {i,}
+        for i, chs in enumerate(zip(s, t)):
+            ch1, ch2 = chs
+            if ch1 not in dict_s:
+                dict_s[ch1] = {i,}
             else:
-                dict_s[ch].add(i)
-        for i, ch in enumerate(t):
-            if ch not in dict_t:
-                dict_t[ch] = {i,}
+                dict_s[ch1].add(i)
+            if ch2 not in dict_t:
+                dict_t[ch2] = {i,}
             else:
-                dict_t[ch].add(i)
+                dict_t[ch2].add(i)
         print(f"{dict_s = }\n{dict_t = }")
         print(f"\n\n{list(dict_s.values()) = }\n{list(dict_t.values()) = }")
         return all(s1 == s2 for s1, s2 in zip(list(dict_s.values()), list(dict_t.values())))
